fe1d4b9e9d06d715b21b2d0ed541ad37e34b1e0b
nwasir <uko20001@byui.edu>
2023-05-24 22:44:02 +0100

    random words

---
+++ week06/scripts/words.js
@@ -0,0 +1,12 @@
+let names = ['John', 'Emily', 'David', 'Sarah', 'Michael'];
+let verbs = ['ate', 'bought', 'planted', 'sold'];
+let fruits = ['apple', 'banana', 'orange', 'mango', 'strawberry'];
+
+function pickRandom() {
+  let name = names[Math.floor(Math.random() * names.length)];
+  let verb = verbs[Math.floor(Math.random() * verbs.length)];
+  let fruit = fruits[Math.floor(Math.random() * fruits.length)];
+
+  let result = `${name} ${verb} a ${fruit}`;
+  document.getElementById('result').textContent = result;
+}
