68afca94c6a589e7f2841c1fc87a11a278ce60ef
Nwasir <uko20001@byui.edu>
2023-10-08 01:37:20 +0100

    week 04 initial

---
+++ week04/script/w04-task.js
@@ -0,0 +1,52 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Nwasir Jim",
+    photo: "images/my-image.jpg",
+    favoriteFoods: [
+        "Beans", 
+        "Ugba", 
+        "Okro soup", 
+        "Ofe akwu", 
+        "Egusi soup", 
+        "Spaghetti"
+    ],
+    hobbies: [
+        "Playing and watching Football",
+        "Coding",
+        "Reading",
+        "singing"
+    ],
+    placesLived: [],
+
+};  
+myProfile.placesLived.push({
+    city: "Port harcourt",
+    lenght: "4 years"
+});
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
